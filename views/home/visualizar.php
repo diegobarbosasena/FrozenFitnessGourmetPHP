da388bc702e6a5b80999eed99095a26f544c0061
JANAINA FERREIRA ANTONIO <15160213@CFP127ALUNO.LOCAL>
2016-10-28 16:45:06 -0200

    dados do cliente

--- views/home/visualizar.php
+++ views/home/visualizar.php
@@ -1,4 +1,102 @@
  <div id="conteudo">
- 
- 
+	
+		 
+	<fieldset class="perfil_cliente">
+		 <legend class="tit_cliente">Meus Dados</legend>
+		      <table class="tbl_dados">
+				<tr>
+					<td class="dados_cliente_tit">Nome:</td>
+					<td class="dados_cliente">Janaina Ferreira Antonio</td>
+					<td class="dados_cliente_tit">Editar</td>
+				</tr>
+				<tr>
+					<td class="dados_cliente_tit">Email:</td>
+					<td class="dados_cliente">janaferreiraa4@gmail.com</td>
+					<td class="dados_cliente_tit">Editar</td>
+				</tr>
+				<tr>
+					<td class="dados_cliente_tit">CPF:</td>
+					<td class="dados_cliente">458.325.654-8</td>
+					<td class="dados_cliente_tit">Editar</td>
+				</tr>
+				<tr>
+					<td class="dados_cliente_tit">Data de nascimento:</td>
+					<td class="dados_cliente">23/12/1999</td>
+					<td class="dados_cliente_tit">Editar</td>
+				</tr>
+				<tr>
+					<td class="dados_cliente_tit">Peso:</td>
+					<td class="dados_cliente">50kg</td>
+					<td class="dados_cliente_tit">Editar</td>
+				</tr>
+				<tr>
+					<td class="dados_cliente_tit">Altura:</td>
+					<td class="dados_cliente">1.50m</td>
+					<td class="dados_cliente_tit">Editar</td>
+				</tr>
+				<tr>
+					<td class="dados_cliente_tit">Celular:</td>
+					<td class="dados_cliente">(11) 98712-5623</td>
+					<td class="dados_cliente_tit">Editar</td>
+				</tr>
+				<tr>
+					<td class="dados_cliente_tit">Telefone:</td>
+					<td class="dados_cliente">(11) 4712-5623</td>
+					<td class="dados_cliente_tit">Editar</td>
+				</tr>
+				<tr>
+					<td class="dados_cliente_tit">Sexo:</td>
+					<td class="dados_cliente">Feminino</td>
+					<td class="dados_cliente_tit">Editar</td>
+				</tr>
+				<tr>
+					<td class="dados_cliente_tit">Objetivo:</td>
+					<td class="dados_cliente">Crescer</td>
+					<td class="dados_cliente_tit">Editar</td>
+				</tr>
+				<tr>
+					<td class="dados_cliente_tit">Endereço:</td>
+					<td class="dados_cliente">Av Carolina de Abreu Paulino</td>
+					<td class="dados_cliente_tit">Editar</td>
+				</tr>
+				<tr>
+					<td class="dados_cliente_tit">Logradouro:</td>
+					<td class="dados_cliente">rua sla oque</td>
+					<td class="dados_cliente_tit">Editar</td>
+				</tr>
+				<tr>
+					<td class="dados_cliente_tit">Rua:</td>
+					<td class="dados_cliente">Janaina Ferreira Antonio</td>
+					<td class="dados_cliente_tit">Editar</td>
+				</tr>
+				<tr>
+					<td class="dados_cliente_tit">Numero:</td>
+					<td class="dados_cliente">Janaina Ferreira Antonio</td>
+					<td class="dados_cliente_tit">Editar</td>
+				</tr>
+				<tr>
+					<td class="dados_cliente_tit">Bairro:</td>
+					<td class="dados_cliente">Janaina Ferreira Antonio</td>
+					<td class="dados_cliente_tit">Editar</td>
+				</tr>
+				<tr>
+					<td class="dados_cliente_tit">CEP:</td>
+					<td class="dados_cliente">Janaina Ferreira Antonio</td>
+					<td class="dados_cliente_tit">Editar</td>
+				</tr>
+				<tr>
+					<td class="dados_cliente_tit">Estado:</td>
+					<td class="dados_cliente">Janaina Ferreira Antonio</td>
+					<td class="dados_cliente_tit">Editar</td>
+				</tr>
+				<tr>
+					<td class="dados_cliente_tit">CEP:</td>
+					<td class="dados_cliente">Janaina Ferreira Antonio</td>
+					<td class="dados_cliente_tit">Editar</td>
+				</tr>
+				
+			  </table>
+	</fieldset>
+	
+	
  </div>
