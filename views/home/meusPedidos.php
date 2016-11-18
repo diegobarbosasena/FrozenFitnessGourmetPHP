d4fc1ca5ea3cecdfeaae73a277403aa0bbb54719
JANAINA FERREIRA ANTONIO <15160213@CFP127ALUNO.LOCAL>
2016-11-18 14:37:17 -0200

    Tela Meus Pedidos

---
+++ views/home/meusPedidos.php
@@ -0,0 +1,44 @@
+<div id="conteudo">
+<fieldset>
+    <p class="subtitulo_sobre">Pedidos em andamento</p>
+    
+    <table class="tbl_dados">
+        <tr>
+            <td class="dados_cliente_tit">Nº Pedido</td>
+            <td class="dados_cliente_tit">Data de compra </td>
+            <td class="dados_cliente_tit">Data de entrega </td>
+            <td class="dados_cliente_tit">Transportadora </td>
+            <td class="dados_cliente_tit">Status </td>
+        </tr>
+        <tr>
+           <td>1</td>
+            <td>12/11/2016</td>
+            <td>20/11/2016</td>
+            <td>The Flash</td>
+            <td>Em andamento</td>
+        </tr>
+    </table>
+</fieldset>
+    <p>
+    
+<fieldset>
+     <p class="subtitulo_sobre">Histórico de pedidos</p>
+    
+    <table class="tbl_dados">
+        <tr>
+            <td class="dados_cliente_tit">Nº Pedido</td>
+            <td class="dados_cliente_tit">Data de compra </td>
+            <td class="dados_cliente_tit">Data de entrega </td>
+            <td class="dados_cliente_tit">Transportadora </td>
+            <td class="dados_cliente_tit">Status </td>
+        </tr>
+        <tr>
+           <td>1</td>
+            <td>12/11/2016</td>
+            <td>20/11/2016</td>
+            <td>The Flash</td>
+            <td>Em andamento</td>
+        </tr>
+    </table>
+    </fieldset>
+</div>
