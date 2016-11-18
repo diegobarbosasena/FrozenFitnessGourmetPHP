6aad603bfb35426b75aa7530e314c5fd10b1a01c
JANAINA FERREIRA ANTONIO <15160213@CFP127ALUNO.LOCAL>
2016-11-18 15:11:23 -0200

    login grande

--- views/home/login.php
+++ views/home/login.php
@@ -1,10 +1,14 @@
 <div id="conteudo">
-    <form class="login-form" method="post" action="<?php  echo PROJECTDIR; ?>usuarios/entrar">
-            <input class="login_caixa" type="text" name="txtusuario" placeholder="username" required/>
-            <input class="login_caixa"type="password" name="txtsenha" placeholder="password" required/>
-            <input class="button_login" type="submit" name="btnLogin" value="Login"/>
+   
+                        <div class="form">                          
+                            <form class="login-form" method="post" action="<?php  echo PROJECTDIR; ?>usuarios/entrar">
+                                <input type="text" name="txtusuario" placeholder="Usuário" required/>
+                                <input type="password" name="txtsenha" placeholder="Senha" required/>
+                                <input class="button_login" type="submit" name="btnLogin" value="Login"/>
                              
-            <p class="message">Não é registrado? <a href="<?php  echo PROJECTDIR; ?>home/cadastro">Crie sua conta</a></p>
-    </form>
+                                <p class="message">Não é registrado? <a href="<?php  echo PROJECTDIR; ?>home/cadastro">Crie sua conta</a></p>
+                            </form>
+                        </div>
+                   
 
 <div>
